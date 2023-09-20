470657a4ed2c71976b0c5ad75b4666fbbc7e2b51
Júlio Mansan <julio.mansan31@gmail.com>
2023-09-20 03:04:41 +0000

    Otimização do JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,66 +1,25 @@
 const date = new Date();
 const weekDay = date.getDay();
-const graphic = document.querySelectorAll('.graphic')
+const graphic = document.querySelectorAll('.graphic');
 
-function mudarCorDoGrafico () {
-    if (weekDay === 0) {
-        graphic[6].style.background = 'hsl(186, 34%, 60%)'
-        graphic[0].style.background = 'hsl(10, 79%, 65%)'
-        graphic[1].style.background = 'hsl(10, 79%, 65%)'
-        graphic[2].style.background = 'hsl(10, 79%, 65%)'
-        graphic[3].style.background = 'hsl(10, 79%, 65%)'
-        graphic[4].style.background = 'hsl(10, 79%, 65%)'
-        graphic[5].style.background = 'hsl(10, 79%, 65%)'
-    } else if (weekDay === 1) {
-        graphic[6].style.background = 'hsl(10, 79%, 65%)'
-        graphic[0].style.background = 'hsl(186, 34%, 60%)'
-        graphic[1].style.background = 'hsl(10, 79%, 65%)'
-        graphic[2].style.background = 'hsl(10, 79%, 65%)'
-        graphic[3].style.background = 'hsl(10, 79%, 65%)'
-        graphic[4].style.background = 'hsl(10, 79%, 65%)'
-        graphic[5].style.background = 'hsl(10, 79%, 65%)'
-    } else if (weekDay === 2) {
-        graphic[6].style.background = 'hsl(10, 79%, 65%)'
-        graphic[0].style.background = 'hsl(10, 79%, 65%)'
-        graphic[1].style.background = 'hsl(186, 34%, 60%)'
-        graphic[2].style.background = 'hsl(10, 79%, 65%)'
-        graphic[3].style.background = 'hsl(10, 79%, 65%)'
-        graphic[4].style.background = 'hsl(10, 79%, 65%)'
-        graphic[5].style.background = 'hsl(10, 79%, 65%)'
-    } else if (weekDay === 3) {
-        graphic[6].style.background = 'hsl(10, 79%, 65%)'
-        graphic[0].style.background = 'hsl(10, 79%, 65%)'
-        graphic[1].style.background = 'hsl(10, 79%, 65%)'
-        graphic[2].style.background = 'hsl(186, 34%, 60%)'
-        graphic[3].style.background = 'hsl(10, 79%, 65%)'
-        graphic[4].style.background = 'hsl(10, 79%, 65%)'
-        graphic[5].style.background = 'hsl(10, 79%, 65%)'
-    } else if (weekDay === 4) {
-        graphic[6].style.background = 'hsl(10, 79%, 65%)'
-        graphic[0].style.background = 'hsl(10, 79%, 65%)'
-        graphic[1].style.background = 'hsl(10, 79%, 65%)'
-        graphic[2].style.background = 'hsl(10, 79%, 65%)'
-        graphic[3].style.background = 'hsl(186, 34%, 60%)'
-        graphic[4].style.background = 'hsl(10, 79%, 65%)'
-        graphic[5].style.background = 'hsl(10, 79%, 65%)'
-    } else if (weekDay === 5) {
-        graphic[6].style.background = 'hsl(10, 79%, 65%)'
-        graphic[0].style.background = 'hsl(10, 79%, 65%)'
-        graphic[1].style.background = 'hsl(10, 79%, 65%)'
-        graphic[2].style.background = 'hsl(10, 79%, 65%)'
-        graphic[3].style.background = 'hsl(10, 79%, 65%)'
-        graphic[4].style.background = 'hsl(186, 34%, 60%)'
-        graphic[5].style.background = 'hsl(10, 79%, 65%)'
-    } else if (weekDay === 6) {
-        graphic[6].style.background = 'hsl(10, 79%, 65%)'
-        graphic[0].style.background = 'hsl(10, 79%, 65%)'
-        graphic[1].style.background = 'hsl(10, 79%, 65%)'
-        graphic[2].style.background = 'hsl(10, 79%, 65%)'
-        graphic[3].style.background = 'hsl(10, 79%, 65%)'
-        graphic[4].style.background = 'hsl(10, 79%, 65%)'
-        graphic[5].style.background = 'hsl(186, 34%, 60%)'
+function mudarCorDoGrafico() {
+    const colors = [
+        'hsl(186, 34%, 60%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)'
+    ];
+
+    for (let i = 0; i < graphic.length; i++) {
+        if (i === weekDay) {
+            graphic[i].style.background = colors[0];
+        } else {
+            graphic[i].style.background = colors[1];
+        }
     }
 }
 
-
-mudarCorDoGrafico()
+mudarCorDoGrafico();
